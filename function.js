46977e3f20a2653c1183223cc272e573031e8568
dramadreaming <dramadreamings2@gmail.com>
2018-10-16 15:20:11 +0900

    Today's wise saying Button Add

---
+++ function.js
@@ -0,0 +1,17 @@
+
+
+var wiseSaying = new Array();
+wiseSaying[0] = 'Lost time is never found again.';
+wiseSaying[1] = 'The key is in not spending time, but in investing it.';
+wiseSaying[2] = 'Only passions, great passions can elevate the soul to great things.';
+wiseSaying[3] = 'Passion is the genesis of genius.';
+wiseSaying[4] = 'Passion is the drunkenness of the mind.';
+
+function ChangeTodayWord()
+{
+  var todayWordText = document.querySelector("#TodayWord");
+  var fRandomValue = Math.random()*5;
+  var nRandomValue = Math.floor(fRandomValue);
+  todayWordText.innerHTML = wiseSaying[nRandomValue];
+
+}

a16cd5948d1042737f77eacd37d88ae9df673cd6
dramadreaming <dramadreamings2@gmail.com>
2018-10-18 21:56:01 +0900

    to-do-list add 기능 추가

--- function.js
+++ function.js
@@ -16,7 +16,29 @@ function ChangeTodayWord()
 
 }
 
-function Func()
+function ClickAddButton()
 {
-    document.write("Func() 호출!!");
+    // div 생성
+    var div = document.createElement('div');
+    div.display = 'none';
+
+    // 체크 이미지 생성
+    var checkImg = document.createElement('img');
+    checkImg.display = 'none';
+    checkImg.src = "./Resource/to_do_check.png";
+    checkImg.style = "width:50"
+
+    // todo의 text를 가져온다
+    var todoText = document.getElementById("TodoText").value;
+    var newText = document.createTextNode(todoText);
+
+    // todo element 생성. text 추가
+    var newToDoElement = document.createElement("span");
+    newToDoElement.appendChild(newText);
+
+    // todo list 에 추가한다
+    var toDoList = document.getElementById("ToDoList");
+    div.appendChild(checkImg);
+    div.appendChild(newToDoElement);
+    toDoList.appendChild(div);
 }

302e414e0b51282115a1d79ca92e956c18185436
dramadreaming <dramadreamings2@gmail.com>
2018-10-19 23:06:40 +0900

    to-do-list item 삭제 이미지 추가

--- function.js
+++ function.js
@@ -7,6 +7,16 @@ wiseSaying[2] = 'Only passions, great passions can elevate the soul to great thi
 wiseSaying[3] = 'Passion is the genesis of genius.';
 wiseSaying[4] = 'Passion is the drunkenness of the mind.';
 
+function CompleteItem()
+{
+  document.write("CompleteItem");
+}
+
+function RemoveItem()
+{
+  document.write("RemoveItem");
+}
+
 function ChangeTodayWord()
 {
   var todayWordText = document.querySelector("#TodayWord");
@@ -27,9 +37,19 @@ function ClickAddButton()
     checkImg.display = 'none';
     checkImg.src = "./Resource/to_do_check.png";
     checkImg.style = "width:50"
+    checkImg.addEventListener("click", CompleteItem);
+
+    // 삭제 이미지 생성
+    var removeImg = document.createElement('img');
+    removeImg.display = 'none';
+    removeImg.src = "./Resource/to_do_cancel.png";
+    removeImg.style = "width:50"
+    removeImg.addEventListener("click", RemoveItem);
+
 
     // todo의 text를 가져온다
-    var todoText = document.getElementById("TodoText").value;
+    var todoInput = document.getElementById("TodoText");
+    var todoText = todoInput.value;
     var newText = document.createTextNode(todoText);
 
     // todo element 생성. text 추가
@@ -39,6 +59,11 @@ function ClickAddButton()
     // todo list 에 추가한다
     var toDoList = document.getElementById("ToDoList");
     div.appendChild(checkImg);
+    div.appendChild(removeImg);
     div.appendChild(newToDoElement);
     toDoList.appendChild(div);
+
+    //값을 없앤다
+    todoInput.value = "";
+
 }
